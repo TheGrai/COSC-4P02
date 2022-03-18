d1f36b23af46c76e710f5b8ff4b5abf122ff6b90
Grant <36864854+TheGrai@users.noreply.github.com>
2022-03-17 22:12:01 -0400

    connection to postgresql database

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chatbot',
+        'USER': 'postgres',
+        'PASSWORD': 'djangobrock',
+        'HOST': 'localhost'
     }
 }
 
