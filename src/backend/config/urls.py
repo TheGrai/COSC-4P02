a7fa99a79771a60820e6106773b323ea438a535f
tk18il <80863707+tk18il@users.noreply.github.com>
2022-04-12 20:29:10 -0400

    Add Backend Chat endpoint to send and recieve messages  (#32)
    * Add Rest Framework
    
    * Add Chat Route to send and recieve messages from chatbot
    
    * Fix static url conflict between Frontend and Backend

--- src/backend/config/urls.py
+++ src/backend/config/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-from django.http import HttpResponse
-
-def homePageView(request):
-    return HttpResponse("Testing Connection to API Complete")
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', homePageView),
-]
+    path('chat/', include('api.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
