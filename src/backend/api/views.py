1bb7706fe9af4bab1d5343f6ee397371875d717c
Grant <36864854+TheGrai@users.noreply.github.com>
2022-04-16 00:54:49 -0400

    Update default Chat API response

--- src/backend/api/views.py
+++ src/backend/api/views.py
@@ -3,5 +3,5 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def generateMessageResponse(request):
-    response = 'Recieved Message: ' + request.query_params['message'] + '. Have a great Day!'
+    response = 'Recieved Message: ' + request.query_params['message']
     return Response({'message': response})
