79f9b135fef566b6bb90a65e9f5aaaa823f65135
Grant <36864854+TheGrai@users.noreply.github.com>
2022-04-16 00:55:12 -0400

    Set up client to display chat API response

--- src/web/src/pages/Chat.jsx
+++ src/web/src/pages/Chat.jsx
@@ -1,138 +1,154 @@
-import { useState, useRef, useEffect } from "react";
-import { ChatBubble } from "../components/ChatBubble";
-import { useNavigate } from "react-router-dom";
-import { format } from "date-fns";
-import { CSVLink } from "react-csv";
+import {useState, useRef, useEffect} from "react";
+import {ChatBubble} from "../components/ChatBubble";
+import {useNavigate} from "react-router-dom";
+import {format} from "date-fns";
+import {CSVLink} from "react-csv";
 
 import "./Chat.css";
 
-export const Chat = ({ name }) => {
-  const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState("");
-  const [latestMessage, setLatestMessage] = useState("");
-  const navigate = useNavigate();
+    export function useFirstRender() {
+        const firstRender = useRef(true);
 
-  const onInputChange = (e) => {
-    setInput(e.target.value);
-  };
+        useEffect(() => {
+            firstRender.current = false;
+        }, []);
 
-  const onInputKeyDown = (e) => {
-    if (input === "") {
-      return;
+        return firstRender.current;
     }
 
-    if (e.key === "Enter") {
-      setLatestMessage(input);
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        {
-          body: input,
-          username: name,
-          timestamp: format(new Date(), "p"),
-          ownedByCurrentUser: true,
-        },
-      ]);
-      setInput("");
-    }
-  };
+export const Chat = ({name}) => {
+        const firstRender = useFirstRender();
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState("");
+    const [latestMessage, setLatestMessage] = useState("");
+    const navigate = useNavigate();
 
-  const handleSendClick = () => {
-    if (input === "") {
-      return;
-    }
+    const onInputChange = (e) => {
+        setInput(e.target.value);
+    };
 
-    setLatestMessage(input);
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      {
-        body: input,
-        username: name,
-        timestamp: format(new Date(), "p"),
-        ownedByCurrentUser: true,
-      },
-    ]);
-    setInput("");
-  };
+    const onInputKeyDown = (e) => {
+        if (input === "") {
+            return;
+        }
 
-  const handleHomeClick = () => navigate("/");
+        if (e.key === "Enter") {
+            setLatestMessage(input);
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                {
+                    body: input,
+                    username: name,
+                    timestamp: format(new Date(), "p"),
+                    ownedByCurrentUser: true,
+                },
+            ]);
+            setInput("");
+        }
+    };
 
-  const chatRef = useRef(null);
+    const handleSendClick = () => {
+        if (input === "") {
+            return;
+        }
 
-  const scrollToBottom = () => {
-    chatRef.current.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-    });
-  };
+        setLatestMessage(input);
+        setMessages((prevMessages) => [
+            ...prevMessages,
+            {
+                body: input,
+                username: name,
+                timestamp: format(new Date(), "p"),
+                ownedByCurrentUser: true,
+            },
+        ]);
+        setInput("");
+    };
 
-  const handleCSVClick = () => {
-    if (!messages.length) {
-      alert("No messages to download");
-      return false;
-    }
-  };
+    const handleHomeClick = () => navigate("/");
 
-  useEffect(() => {
-    // fetch(`http://localhost:2022/chat/?message=${latestMessage}`).then(() => console.log("finish")).then((message) => {
-    //   setMessages((prevMessages) => [
-    //     ...prevMessages,
-    //     {
-    //       body: message,
-    //       username: "the enemy",
-    //       timestamp: format(new Date(), "p"),
-    //       ownedByCurrentUser: false,
-    //     },
-    //   ]);
-    // }).catch(() => console.log("error"));
-    const getResponse = async() => {
-      const results = fetch(`http://localhost:2022/chat/?message=${latestMessage}`)
-      console.log(results);
-    }
-    getResponse();
-  }, [latestMessage, setMessages]);
+    const chatRef = useRef(null);
 
-  useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
+    const scrollToBottom = () => {
+        chatRef.current.scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+        });
+    };
 
-  useEffect(() => {
-    if (name === "") {
-      navigate("/");
-    }
-  }, [name]);
+    const handleCSVClick = () => {
+        if (!messages.length) {
+            alert("No messages to download");
+            return false;
+        }
+    };
 
-  return (
-    <div className="chat">
-      <div className="header">
-        <p>Chat room</p>
-        <button className="back-btn" onClick={handleHomeClick}>
-          Home
-        </button>
-      </div>
-      <div className="chat-container">
-        {messages.map((props) => (
-          <ChatBubble {...props} />
-        ))}
-        <div ref={chatRef} />
-      </div>
-      <div className="input-container">
-        <input
-          className="chat-input"
-          value={input}
-          onChange={onInputChange}
-          onKeyDown={onInputKeyDown}
-        />
-        <button onClick={handleSendClick} className="send-btn">
-          send
-        </button>
-        <CSVLink
-          data={messages}
-          className="send-btn download-csv-btn"
-          onClick={handleCSVClick}
-        >
-          &#8595;
-        </CSVLink>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        if(!firstRender){
+            const getResponse = async () => {
+                fetch(`http://localhost:2022/chat/?message=${latestMessage}`)
+                    .then(response => response.json())
+                    .then(data => {
+                            setMessages((prevMessages) => [
+                                ...prevMessages,
+                                {
+                                    body: data.message,
+                                    username: 'Dr. Bot',
+                                    timestamp: format(new Date(), "p"),
+                                    ownedByCurrentUser: false,
+                                },
+                            ]);
+                            console.log(data.message);
+                        }
+                    );
+            }
+            getResponse();
+
+        }
+    }, [latestMessage, setMessages]);
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
+
+    useEffect(() => {
+        if (name === "") {
+            navigate("/");
+        }
+    }, [name]);
+
+    return (
+        <div className="chat">
+            <div className="header">
+                <p>Chat room</p>
+                <button className="back-btn" onClick={handleHomeClick}>
+                    Home
+                </button>
+            </div>
+            <div className="chat-container">
+                {messages.map((props) => (
+                    <ChatBubble {...props} />
+                ))}
+                <div ref={chatRef}/>
+            </div>
+            <div className="input-container">
+                <input
+                    className="chat-input"
+                    value={input}
+                    onChange={onInputChange}
+                    onKeyDown={onInputKeyDown}
+                />
+                <button onClick={handleSendClick} className="send-btn">
+                    send
+                </button>
+                <CSVLink
+                    data={messages}
+                    className="send-btn download-csv-btn"
+                    onClick={handleCSVClick}
+                >
+                    &#8595;
+                </CSVLink>
+            </div>
+        </div>
+    );
 };
