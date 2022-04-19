be0dbbdadb65ba14d7371f724a258dbb718bf0ad
tk18il <80863707+tk18il@users.noreply.github.com>
2022-04-19 18:11:17 -0400

    Add Models for Information about Brock Courses and Instructors (#38)
    * Inital Data Dump
    
    * Brock Partial Data Dump Complete

--- src/backend/brockU/admin.py
+++ src/backend/brockU/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from import_export import fields, resources
+from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import ForeignKeyWidget, JSONWidget
+from .models import Subject, Course, CourseOffering, Instructor
 
 # Register your models here.
+class SubjectResource(resources.ModelResource):
+    class Meta:
+        model = Subject
+
+class SubjectAdmin(ImportExportModelAdmin):
+    resource_class = SubjectResource
+
+admin.site.register(Subject, SubjectAdmin)
+
+class InstructorResource(resources.ModelResource):
+    class Meta:
+        model = Instructor
+
+class InstructorAdmin(ImportExportModelAdmin):
+    resource_class = InstructorResource
+
+admin.site.register(Instructor, InstructorAdmin)
+
+class CourseResource(resources.ModelResource):
+    class Meta:
+        model = Course
+
+class CourseAdmin(ImportExportModelAdmin):
+    resource_class = CourseResource
+
+admin.site.register(Course, CourseAdmin)
+
+class CourseOfferingResource(resources.ModelResource):
+    def before_import_row(self, row, row_number=None, **kwargs):
+        first_name = row["instructor_first_name"]
+        last_name = row["instructor_last_name"]
+        instructorQuery =  Instructor.objects.filter(first_name=first_name, last_name=last_name)
+        if instructorQuery:
+            instructor_id = instructorQuery[0].id
+            row['instructor_id'] = instructor_id
+
+    course_id = fields.Field(
+        column_name='code',
+        attribute='course_id',
+        widget=ForeignKeyWidget(Course, field='code')
+    )
+    schedule = fields.Field(
+        column_name='schedule',
+        attribute='schedule',
+        widget=JSONWidget()
+    )
+    class Meta:
+        model = CourseOffering
+        exclude = ('code', )
+
+class CourseComponentAdmin(ImportExportModelAdmin):
+    resource_class = CourseOfferingResource
+
+admin.site.register(CourseOffering, CourseComponentAdmin)
--- src/backend/brockU/models.py
+++ src/backend/brockU/models.py
@@ -1,3 +1,72 @@
+import uuid
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class Subject(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=4, unique=True)
+    name = models.CharField(max_length=64)
+    url = models.URLField(blank=True)
+
+class Instructor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+
+def validate_course_code(value):
+    course_code = value.split()
+    credit_value = ['F', 'N', 'P', 'Q', 'Y']
+    try:
+        subject = Subject.objects.get(code = course_code[0])
+        creditIndex = credit_value.index(course_code[1][1])
+    except Subject.DoesNotExist:
+        raise ValidationError(
+            _('%(value)s is not a valid Subject.'),
+            params = {'value': course_code[0]},
+        )
+    except:
+        raise ValidationError(
+            _('%(value)s is not a valid Course Code.'),
+            params = {'value': value},
+        )
+
+class Course(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=9, validators=[validate_course_code])
+    name = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+    format = models.TextField(blank=True)
+    prerequesites = models.TextField(blank=True)
+    exclusions = models.TextField(blank=True)
+    restrictions = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
 
 # Create your models here.
+class CourseOffering(models.Model):
+    class DeliveryType(models.TextChoices):
+        ASYNCHRONOUS = 'ASY', _('Asynchronous Online')
+        BLENDED = 'BLD', _('Blended')
+        PRACTICUM = 'CLI', _('Practicum (Clinic)')
+        FIELD_COURSE = 'FLD', _('Field Course')
+        HYFLEX = 'HYF', _('Hyflex')
+        INTERNATIONAL_FIELD_EXPERIENCE = 'IFT', _('International Field Experience')
+        INTERNSHIP = 'INT', _('Internship')
+        LABORATORY = 'LAB', _('Labortory')
+        LECTURE = 'LEC', _('Lecture')
+        ONLINE_MIXED = 'ONM', _('Online Mixed')
+        PROJECT = 'PRO', _('Project')
+        SEMINAR = 'SEM', _('Seminar')
+        SYNCHRONOUS = 'SYN', _('Synchronus')
+        TUTORIAL = 'TUT', _('Tutorial')
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    course_id = models.ForeignKey('Course', on_delete=models.CASCADE)
+    delivery_type = models.CharField(choices=DeliveryType.choices, default=DeliveryType.LECTURE, max_length=3)
+    instructor_id = models.ForeignKey('Instructor', null=True, on_delete=models.SET_NULL)
+    duration = models.CharField(max_length=3)
+    section = models.IntegerField()
+    location = models.CharField(max_length=64, blank=True)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    schedule = models.JSONField(blank=True)
