a3bd8f8780f55fb68027b1f115b26e5373847560
Grant <36864854+TheGrai@users.noreply.github.com>
2022-04-19 18:12:12 -0400

    Fix spelling mistake

--- src/backend/api/views.py
+++ src/backend/api/views.py
@@ -3,5 +3,5 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def generateMessageResponse(request):
-    response = 'Recieved Message: ' + request.query_params['message']
+    response = 'Received Message: ' + request.query_params['message']
     return Response({'message': response})
