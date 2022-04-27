30ab1514abdd91f99b876dbec8b89fbe93a5441f
tk18il <80863707+tk18il@users.noreply.github.com>
2022-04-27 16:19:59 -0400

    Frontend Changes (#56)
    * Add Thinking bubble to inform chatbot is thinking
    
    * Add Random Background Image Generation

---
+++ src/web/src/components/ChatTyping.jsx
@@ -0,0 +1,9 @@
+import "./ChatTyping.css";
+
+export const Typing = () => (
+  <div className="typing">
+    <div className="typing__dot"></div>
+    <div className="typing__dot"></div>
+    <div className="typing__dot"></div>
+  </div>
+)
